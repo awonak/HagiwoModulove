3bae7b07efd2e89276f977148d5fede81cbc1267
Adam Wonak <adam.wonak@gmail.com>
2023-08-10 17:47:53 -0500

    Add UI docs to the main docstring.

--- A-RYTH-MATIK/Certainty/Certainty.ino
+++ A-RYTH-MATIK/Certainty/Certainty.ino
@@ -13,6 +13,16 @@
  * encoder to randomize the seed or adjust the pattern length. The user
  * configurable parameters of seed and step length will be saved to EEPROM and
  * will be recalled the next time you power on the module.
+ * 
+ * Encoder:
+ *      short press: Toggle between editing the step length and selecting a seed.
+ *      long press: Enter seed edit mode to manually provide a seed value.
+ * 
+ * CLK: Provide a gate or trigger for each output to repeat with decreasing
+ *      probability in each output.
+ * 
+ * RST: Trigger this input to reseed the psudo random sequence.
+ * 
  */
 
 // Oled setting
