2bca8f969d0796655cfd6919accb9f825db25fe6
Adam Wonak <adam.wonak@gmail.com>
2024-06-29 12:09:47 -0500

    add libmodulove for SyncLFO

--- SyncLFO/Baby4/Baby4.ino
+++ SyncLFO/Baby4/Baby4.ino
@@ -9,50 +9,32 @@
  *
  */
 
-#include <avr/io.h>
+#include "src/libmodulove/synclfo.h"
 
-// GPIO Pin mapping.
-#define P1 0  // Step 1
-#define P2 1  // Step 2
-#define P3 3  // Step 3
-#define P4 5  // Step 4
+using namespace modulove;
+using namespace synclfo;
 
-#define TRIG_IN 3  // Trigger Input to advance step
-#define CV_OUT 10  // CV Output for current step
 
-bool clk = 1;  // External trigger input detect
-bool old_clk = 0;
+// Declare SyncLFO hardware variable.
+SyncLFO hw;
 
-const byte P[] = {P1, P2, P3, P4};  // Array of knob GPIO pin identifiers.
-
-const byte max_val = 255;  // CV step max value
-const byte max_step = 4;   // Max steps in the CV sequence (corresponding to number of knobs)
-
-byte val = 0;   // Current CV value
-byte step = 0;  // Current CV step (0,1,2,3)
-int read = 0;   // Raw cv read from pot for current step
+// Current CV step (0,1,2,3)
+byte step = 0;
 
 void setup() {
-    pinMode(TRIG_IN, INPUT);  // Trigger in
-    pinMode(CV_OUT, OUTPUT);  // Current cv step out
-
-    // Register setting for high frequency PWM.
-    TCCR1A = 0b00100001;
-    TCCR1B = 0b00100001;
-
-    delay(100);
+    // Initialize the SyncLFO peripherials.
+    hw.Init();
 }
 
 void loop() {
-    old_clk = clk;
-    clk = digitalRead(TRIG_IN);
+    // Read cv inputs to determine state for this loop.
+    hw.ProcessInputs();
 
     // Detect if new trigger received and advance step.
-    if (old_clk == 0 && clk == 1) {
-        step = (step + 1) % max_step;
+    if (hw.trig.State() == DigitalInput::STATE_RISING) {
+        step = (step + 1) % synclfo::P_COUNT;
     }
 
     // Write current step CV output.
-    val = map(analogRead(P[step]), 0, 1023, 0, max_val);
-    analogWrite(CV_OUT, val);
+    hw.output.Value(hw.knobs[step].Read());
 }
