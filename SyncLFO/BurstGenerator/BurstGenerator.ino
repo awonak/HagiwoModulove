08963f10752d9eeffa4d6df69bfb443aeb5e0701
Adam Wonak <adam.wonak@gmail.com>
2024-07-02 22:31:23 -0500

    Update BurstGenerator to use libmodulove

--- SyncLFO/BurstGenerator/BurstGenerator.ino
+++ SyncLFO/BurstGenerator/BurstGenerator.ino
@@ -2,7 +2,7 @@
  * @file BurstGenerator.ino
  * @author Adam Wonak (https://github.com/awonak/)
  * @brief
- * @version 0.1
+ * @version 0.2
  * @date 2024-04-08
  *
  * @copyright Copyright (c) 2024
@@ -16,25 +16,21 @@
  *
  */
 
+#include <synclfo.h>
+
 // ALTERNATE HARDWARE CONFIGURATION
-// #define SYNCHRONIZER
+#define SYNCHRONIZER
 
-// GPIO Pin mapping.
-#define P1 A0  // Burst Rate
-#define P2 A1  // Burst Count
-#define P3 A3  // Full Burst Slope (fade in, flat, fade out)
-#define P4 A5  // Individual Burst Slope (fade in, flat, fade out)
+// Debug flag
+// #define DEBUG
 
-#define DIGITAL_IN 3  // Trigger Input to advance step
-#define CV_OUT 10     // CV Output for current step
+using namespace modulove;
+using namespace synclfo;
+
+// Declare SyncLFO hardware variable.
+SyncLFO hw;
 
 #ifdef SYNCHRONIZER
-#define B1 A2    // Trigger burst
-#define B2 12    // Retrig / Loop
-#define LED1 11  // Slider LED 1
-#define LED2 10  // Slider LED 2
-byte button1_state;
-byte button2_state;
 unsigned long _last_press;
 const int DEBOUNCE_MS = 10;
 #endif
@@ -48,8 +44,8 @@ const int MAX_BURST_RATE = 512;
 
 // Script state.
 int counter = 0;
-byte individual_cv = 0;
-byte output = 0;
+int individual_cv = 0;
+int output = 0;
 float individual_progress = 0;
 float full_progress = 0;
 unsigned long burst_start = 0;
@@ -75,26 +71,21 @@ enum TrigShape {
 TrigShape trig_shape = TRIG_FLAT;
 
 void setup() {
-    pinMode(DIGITAL_IN, INPUT);
-    pinMode(CV_OUT, OUTPUT);
-    pinMode(P1, INPUT);
-    pinMode(P2, INPUT);
-    pinMode(P3, INPUT);
-    pinMode(P4, INPUT);
-
+#ifdef DEBUG
+    Serial.begin(115200);
+#endif
 #ifdef SYNCHRONIZER
-    pinMode(B1, INPUT_PULLUP);
-    pinMode(B2, INPUT_PULLUP);
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
+    hw.config.Synchronizer = true;
 #endif
 
-    // Register setting for high frequency PWM.
-    TCCR1A = 0b00100001;
-    TCCR1B = 0b00100001;
+    // Initialize the SyncLFO peripherials.
+    hw.Init();
 }
 
 void loop() {
+    // Read cv inputs to determine state for this loop.
+    hw.ProcessInputs();
+
     // Detect if a new trigger has been received. If so, read inputs for burst
     // settings.
     if (beginBurst()) {
@@ -103,11 +94,10 @@ void loop() {
         counter = 0;
 
         // Read the current knob parameters.
-        burst_rate =
-            map(analogRead(P1), 0, 1024, MIN_BURST_RATE, MAX_BURST_RATE);
-        burst_count = map(analogRead(P2), 0, 1023, 1, MAX_BURST_COUNT);
-        sequence_shape = readSequenceShape(P3);
-        trig_shape = readTrigShape(P4);
+        burst_rate = map(hw.p1.Read(), 0, MAX_INPUT, MIN_BURST_RATE, MAX_BURST_RATE);
+        burst_count = map(hw.p2.Read(), 0, MAX_INPUT, 1, MAX_BURST_COUNT);
+        sequence_shape = readSequenceShape(hw.p3.Read());
+        trig_shape = readTrigShape(hw.p4.Read());
     }
 
     // Update burst cv if withing a bursting state.
@@ -122,7 +112,7 @@ void loop() {
                 full_progress = float(millis() - burst_start) /
                                 float(((2 * burst_rate) * burst_count));
                 // Max output or random step.
-                target_cv = (sequence_shape == SEQUENCE_RANDOM) ? random(255) : 255;
+                target_cv = (sequence_shape == SEQUENCE_RANDOM) ? random(MAX_INPUT) : MAX_INPUT;
             }
         }
 
@@ -156,70 +146,46 @@ void loop() {
                 output = (individual_cv - (full_progress * individual_cv));
                 break;
             case SEQUENCE_TRIANGLE:
-                output = (counter <= burst_count) 
-                    ? (full_progress * individual_cv)
-                    : (individual_cv - (full_progress * individual_cv));
+                output = (counter <= burst_count)
+                             ? (full_progress * individual_cv)
+                             : (individual_cv - (full_progress * individual_cv));
         }
 
-        analogWrite(CV_OUT, output);
-    }
-    else {
-        analogWrite(CV_OUT, 0);
+        hw.output.Update(output);
+    } else {
+        hw.output.Update(0);
     }
 }
 
 bool beginBurst() {
-    static int old_din;
-    bool begin_burst;
-
     // Read all inputs.
-    int new_din = digitalRead(DIGITAL_IN);
-    begin_burst = new_din == 1 && old_din == 0;
-    old_din = new_din;
+    bool trigger_start = hw.trig.State() == DigitalInput::STATE_RISING;
 
 #ifdef SYNCHRONIZER
-    // Echo digital input on LED 1.
-    digitalWrite(LED1, old_din);
-
-    // Read current button state.
-    bool new_button1_state = digitalRead(B1);
-
-    // State check with debounce.
-    bool _debounced = (millis() - _last_press) > DEBOUNCE_MS;
-    bool _pressed = button1_state == 1 && new_button1_state == 0 && _debounced;
-    bool _released = button1_state == 0 && new_button1_state == 1 && _debounced;
-
-    // If the button has been pressed, set trigger_start to True if it isn't
-    // already.
-    begin_burst |= _pressed;
-
-    // Update variables for next loop
-    button1_state = new_button1_state;
-    _last_press = (_pressed || _released) ? millis() : _last_press;
+    trigger_start |= hw.b1.Change() == Button::CHANGE_PRESSED;
 #endif
 
-    return begin_burst;
+    return trigger_start;
 }
 
-SequenceShape readSequenceShape(uint8_t pin) {
-    int read = analogRead(pin);
-    if (read <= 204) {
+SequenceShape readSequenceShape(int val) {
+    if (val <= 204) {
         return SEQUENCE_FADE_IN;
-    } else if (read <= 410) {
+    } else if (val <= 410) {
         return SEQUENCE_FLAT;
-    } else if (read <= 616) {
+    } else if (val <= 616) {
         return SEQUENCE_FADE_OUT;
-    } else if (read <= 820) {
+    } else if (val <= 820) {
         return SEQUENCE_TRIANGLE;
     } else {
         return SEQUENCE_RANDOM;
     }
 }
 
-TrigShape readTrigShape(uint8_t pin) {
-    if (analogRead(pin) <= 341) {
+TrigShape readTrigShape(int val) {
+    if (val <= 341) {
         return TRIG_FADE_IN;
-    } else if (analogRead(pin) <= 682) {
+    } else if (val <= 682) {
         return TRIG_FLAT;
     } else {
         return TRIG_FADE_OUT;
